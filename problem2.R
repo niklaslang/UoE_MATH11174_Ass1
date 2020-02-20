91e385581729574f2dbfbceb354aabedbe070902
Niklas Lang <niklasjonathanlang@gmail.com>
2020-02-20 11:37:53 +0000

    problem2 - c - 100%

--- problem2.R
+++ problem2.R
@@ -7,7 +7,7 @@ gfr.2 <- read.csv("assignment1_data/ltegfr2.csv", stringsAsFactors = TRUE)
 
 # converting the files to data tables and merge in an appropriate way into a single data table
 
-eGFR.dt <- merge( data.table(gfr.1),
+eGFR.dt <- merge(data.table(gfr.1),
                  data.table(gfr.2),
                  by.x = c("id","fu.years"), 
                  by.y = c("ID","fu.years"),
@@ -27,41 +27,14 @@ stopifnot( all(eGFR.dt[, c("id", "fu.years")] == eGFR.dt[order(id, fu.years), fu
 
 summary.egfr.table <- NULL
 
-# creating bins for eGFR ranges:
-CKD.stage.1 <- 0
-CKD.stage.2 <- 0
-CKD.stage.3 <- 0
-CKD.stage.4 <- 0
-CKD.stage.5 <- 0
-
-# creating bin for missing average eGFR
-missing.avg.egfr <- 0
-
 for (pat.id in 1:max(eGFR.dt$id)){
   
   # calculate average eGFR for each patient
-  avg.egfr <- mean(eGFR.dt[id == pat.id]$egfr)
-  
+  avg.egfr <- mean(eGFR.dt[id == pat.id]$egfr, na.rm = TRUE)
   # calculate average follow-up for each patient
-  avg.fu.years <- mean(eGFR.dt[id == pat.id]$fu.years)
-  
+  avg.fu.years <- mean(eGFR.dt[id == pat.id]$fu.years, na.rm = TRUE)
   # create a summary table
   summary.egfr.table <- rbind(summary.egfr.table, c(pat.id, avg.egfr, avg.fu.years))
-  
-  # increment the respective bin counter by 1
-  if ( is.na(avg.egfr) ){
-    missing.avg.egfr <- missing.avg.egfr + 1
-  } else if (avg.egfr > 0 && avg.egfr <= 15){
-    CKD.stage.5 <- CKD.stage.5 + 1
-  } else if (avg.egfr > 15 && avg.egfr <= 30){
-    CKD.stage.4 <- CKD.stage.4 + 1
-  } else if (avg.egfr > 30 && avg.egfr <= 60){
-    CKD.stage.3 <- CKD.stage.3 + 1
-  } else if (avg.egfr > 60 && avg.egfr <= 90){
-    CKD.stage.2 <- CKD.stage.2 + 1
-  } else if (avg.egfr > 90){
-    CKD.stage.1 <- CKD.stage.1 + 1
-  }
 }
 
 colnames(summary.egfr.table) <- c("pat.id", "avg.gfr", "avg.fu.years") # assign colnames to table
@@ -70,18 +43,14 @@ head(summary.egfr.dt)
 
 # tabulating the number of patients with average eGFR in the following ranges: 
 # (0, 15], (15, 30], (30, 60], (60, 90], (90, ∞)
-CKD.stages.dt <- data.table("CKD stage" = c("I","II","III","IV","V"), 
-                            "N Patients" = c(CKD.stage.1, CKD.stage.2, CKD.stage.3, CKD.stage.4, CKD.stage.5))
-CKD.stages.dt
+egfr.bins <- c(0, 15, 30, 60, 90, Inf)
+table(cut(summary.egfr.dt$avg.gfr, egfr.bins))
 
 # Counting and reporting the number of patients with missing average eGFR: 
-stopifnot(sum(is.na(summary.egfr.dt$avg.gfr)) == missing.avg.egfr)
 sum(is.na(summary.egfr.dt$avg.gfr))
 missing.avg.egfr
 
 # Ensuring that the table ordering is returned to id, and follow-up time
-
-# WHICH TABLE?
 
 # eGFR.dt
 setorderv(eGFR.dt, c("id","fu.years"), c(1,1))
@@ -114,6 +83,10 @@ summary.egfr.dt <- cbind(summary.egfr.dt, lr.models)
 # Counting how many patients have a slope < -3, [-3, 0), [0, 3], > 3
 
 # Creating slope bins
+
+slope.bins <- c(-Inf,-3,0,3,Inf)
+table(cut(coef(summary.egfr.dt$lr.models[[1]])[2], slope.bins))
+
 slope.1 <- 0 # < -3
 slope.2 <- 0 # [-3, 0)
 slope.3 <- 0 # [0, 3]
@@ -141,3 +114,33 @@ for (id in 1:max(summary.egfr.dt$pat.id)){
 slopes.dt <- data.table("slope" = c("< -3","[-3, 0)","[0, 3]","> 3"), 
                         "N Patients" = c(slope.1, slope.2, slope.3, slope.4))
 slopes.dt
+
+### (c) ###
+
+# For patients with average eGFR in the (0,15] range, 
+# collect in a data table their identifier, sex, age at baseline, average eGFR, 
+# time of last eGFR reading and number of eGFR measurements taken
+
+eGFR.le.15 <- NULL
+
+for (i in 1:max(summary.egfr.dt$pat.id)){
+  avg.gfr <- summary.egfr.dt[pat.id == i]$avg.gfr
+
+  if (!is.na(avg.gfr) && avg.gfr > 0 && avg.gfr <= 15){
+    sex <- eGFR.dt[id == i]$sex[1]
+    age <- eGFR.dt[id == i]$baseline.age[1]
+    last.egfr <- last(eGFR.dt[id == i]$fu.years)
+    egfr.measurements <- sum(!is.na(eGFR.dt[id == i]$egfr))
+    
+    eGFR.le.15 <- rbind(eGFR.le.15, c(i, sex, age, avg.gfr, last.egfr, egfr.measurements))
+
+  }
+  
+  
+}
+
+eGFR.le.15.dt <- data.table(eGFR.le.15)
+colnames(eGFR.le.15.dt) <- c('Patient ID', 'Sex', 'Age (baseline)', 'average eGFR', 'last eGFR measurments', 'eGFR measurements')
+
+head(eGFR.le.15.dt)
+
