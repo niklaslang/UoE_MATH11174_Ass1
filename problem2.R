38794d163da815829686c846edda3f0557858caf
Niklas Lang <niklasjonathanlang@gmail.com>
2020-02-20 10:18:56 +0000

    problem2 - a - 100%

--- problem2.R
+++ problem2.R
@@ -39,37 +39,44 @@ missing.avg.egfr <- 0
 
 for (pat.id in 1:max(eGFR.dt$id)){
   
-  avg.egfr <- mean(eGFR.dt[id == pat.id]$egfr) 
+  # calculate average eGFR for each patient
+  avg.egfr <- mean(eGFR.dt[id == pat.id]$egfr)
+  
+  # calculate average follow-up for each patient
   avg.fu.years <- mean(eGFR.dt[id == pat.id]$fu.years)
   
+  # create a summary table
   summary.egfr.table <- rbind(summary.egfr.table, c(pat.id, avg.egfr, avg.fu.years))
   
+  # increment the respective bin counter by 1
   if ( is.na(avg.egfr) ){
     missing.avg.egfr <- missing.avg.egfr + 1
-  } else if (avg.egfr >= 0 && avg.egfr < 15){
-    CKD.stage.1 <- CKD.stage.1 + 1
-  } else if (avg.egfr >= 15 && avg.egfr < 30){
-    CKD.stage.2 <- CKD.stage.2 + 1
-  } else if (avg.egfr >= 30 && avg.egfr < 60){
-    CKD.stage.3 <- CKD.stage.3 + 1
-  } else if (avg.egfr >= 60 && avg.egfr < 90){
-    CKD.stage.4 <- CKD.stage.4 + 1
-  } else if (avg.egfr > 90){
+  } else if (avg.egfr > 0 && avg.egfr <= 15){
     CKD.stage.5 <- CKD.stage.5 + 1
+  } else if (avg.egfr > 15 && avg.egfr <= 30){
+    CKD.stage.4 <- CKD.stage.4 + 1
+  } else if (avg.egfr > 30 && avg.egfr <= 60){
+    CKD.stage.3 <- CKD.stage.3 + 1
+  } else if (avg.egfr > 60 && avg.egfr <= 90){
+    CKD.stage.2 <- CKD.stage.2 + 1
+  } else if (avg.egfr > 90){
+    CKD.stage.1 <- CKD.stage.1 + 1
   }
 }
 
-summary.egfr.dt <- data.table(summary.egfr.table)
-colnames(summary.egfr.dt) <- c("pat.id", "avg.gfr", "avg.fu.years")
+colnames(summary.egfr.table) <- c("pat.id", "avg.gfr", "avg.fu.years") # assign colnames to table
+summary.egfr.dt <- data.table(summary.egfr.table) # create data.table
+head(summary.egfr.dt)
 
 # tabulating the number of patients with average eGFR in the following ranges: 
 # (0, 15], (15, 30], (30, 60], (60, 90], (90, ∞)
-CKD.stages.dt <- data.table("CKD stage" = c(1,2,3,4,5), 
+CKD.stages.dt <- data.table("CKD stage" = c("I","II","III","IV","V"), 
                             "N Patients" = c(CKD.stage.1, CKD.stage.2, CKD.stage.3, CKD.stage.4, CKD.stage.5))
+CKD.stages.dt
 
 # Counting and reporting the number of patients with missing average eGFR: 
-stopifnot(sum(is.na(avg.dt$avg.gfr)) == missing.avg.egfr)
-sum(is.na(avg.dt$avg.gfr))
+stopifnot(sum(is.na(summary.egfr.dt$avg.gfr)) == missing.avg.egfr)
+sum(is.na(summary.egfr.dt$avg.gfr))
 missing.avg.egfr
 
 # Ensuring that the table ordering is returned to id, and follow-up time
@@ -80,9 +87,9 @@ missing.avg.egfr
 setorderv(eGFR.dt, c("id","fu.years"), c(1,1))
 stopifnot( all(eGFR.dt[, c("id", "fu.years")] == eGFR.dt[order(id, fu.years), fu.years, by = id] ))
 
-# avg.dt
-setorderv(avg.dt, c("pat.id","avg.fu.years"), c(1,1))
-stopifnot( all(avg.dt[, c("pat.id", "avg.fu.years")] == avg.dt[order(pat.id, avg.fu.years), avg.fu.years, by = pat.id] ))
+# summary.egfr.dt
+setorderv(summary.egfr.dt, c("pat.id","avg.fu.years"), c(1,1))
+stopifnot( all(summary.egfr.dt[, c("pat.id", "avg.fu.years")] == summary.egfr.dt[order(pat.id, avg.fu.years), avg.fu.years, by = pat.id] ))
 
 # Fitting a linear regression model for the eGFR measurements as a function of time
 # for each patient with at least 3 eGFR readings,
@@ -99,8 +106,38 @@ for (pat.id in 1:max(eGFR.dt$id)){
   }
   
   lr.models[[pat.id]] <- regr.egfr
+  
 }
 
 summary.egfr.dt <- cbind(summary.egfr.dt, lr.models)
 
-# Couning how many patients have a slope < -3, [-3, 0), [0, 3], > 3
+# Counting how many patients have a slope < -3, [-3, 0), [0, 3], > 3
+
+# Creating slope bins
+slope.1 <- 0 # < -3
+slope.2 <- 0 # [-3, 0)
+slope.3 <- 0 # [0, 3]
+slope.4 <- 0 # > 3
+
+# Looping over pat.id 
+for (id in 1:max(summary.egfr.dt$pat.id)){
+  
+  if (!is.na(summary.egfr.dt[pat.id == id ]$lr.models)){
+    
+    slope <- coef(summary.egfr.dt[pat.id == id ]$lr.models[[1]])[2]
+    
+    if (slope < -3){
+      slope.1 <- slope.1 + 1
+    } else if (slope >= -3 && slope < 0){
+      slope.2 <- slope.2 + 1
+    } else if (slope >= 0 && slope <= 3){
+      slope.3 <- slope.3 + 1
+    } else if (slope > 3){
+      slope.4 <- slope.4 + 1
+    }
+  }
+}
+
+slopes.dt <- data.table("slope" = c("< -3","[-3, 0)","[0, 3]","> 3"), 
+                        "N Patients" = c(slope.1, slope.2, slope.3, slope.4))
+slopes.dt
