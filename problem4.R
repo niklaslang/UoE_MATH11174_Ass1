e52c0a36333bc6f99c657b255340041784c45217
Niklas Lang <niklasjonathanlang@gmail.com>
2020-02-25 18:08:30 +0000

    problem4 - b - 100%

--- problem4.R
+++ problem4.R
@@ -20,6 +20,14 @@ signif(pchisq(M1$null.deviance - M1$deviance, df=1, lower.tail=FALSE), 3)
 
 # Fit a second model (M2) by adding the number of spontaneous abortions 
 # to the set of predictors used in model M1
-# Report odds ratio and 95% CI for the spontaneuous abortions variable
-# Perform a likelihood ratio test to compare model M2 to model M1
-# and report the p-value for the test.
+
+M2 <- glm(case ~ age + parity + spontaneous, data = infert.dt, family="binomial")
+
+# odds ratio and 95% CI for the spontaneous abortions variable
+M2.abortions <- exp(coef(M2)[4])
+CI.M2.abortions <- exp(confint(M2)[4, ])
+round(c(M2.abortions, CI.M2.abortions),2)
+
+# Reporting a p-value for the likelihood ratio test compare model M2 to model M1
+pval <- pchisq(M1$deviance - M2$deviance, df=1, lower.tail=FALSE)
+signif(pval, 2)
